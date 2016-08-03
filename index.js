3d364131f7338e16815375d750d38eb763bbddf9
Nick Ooms <oomsni@gmail.com>
2016-08-03 16:56:17 +0200

    g

--- index.js
+++ index.js
@@ -82,6 +82,16 @@ for (var i = 0; i < langs.length; i++) {
 select_language.selectedIndex = 6;
 updateCountry();
 select_dialect.selectedIndex = 6;
+function updateCountry() {
+  for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+    select_dialect.remove(i);
+  }
+  var list = langs[select_language.selectedIndex];
+  for (var i = 1; i < list.length; i++) {
+    select_dialect.options.add(new Option(list[i][1], list[i][0]));
+  }
+  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+}
 
     recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
